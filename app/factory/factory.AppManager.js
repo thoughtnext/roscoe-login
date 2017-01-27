a601c2270e571573afa67ef02a042affb2cfb02f
amit-thoughtnext <amit@thoughtnext.com>
2017-01-27 21:03:09 +0530

    Update factory.AppManager.js

--- app/factory/factory.AppManager.js
+++ app/factory/factory.AppManager.js
@@ -52,7 +52,7 @@
         return deferred.promise;
       },
       authorize: function(PSID, EMAIL, PASSWORD, FNAME, LNAME, GENDER) {
-        var baseUrl = 'https://f6d8ffc8.ngrok.io' 
+        var baseUrl = 'https://0dbfee9d.ngrok.io' 
         var deferred = $q.defer();
         // var options = {
         // method: 'GET',
